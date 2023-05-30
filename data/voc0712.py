305c3fe80deff48d0420700e34c0312c8c3e2b96
noda-r <119392677+noda-r@users.noreply.github.com>
2023-05-30 23:04:36 +0900

    Update voc0712.py

--- data/voc0712.py
+++ data/voc0712.py
@@ -147,10 +147,9 @@ class VOCDetection(data.Dataset):
         img = cv2.imread(self._imgpath % img_id)
         
         height, width, channels = img.shape
-        
+        print("self.target_transform",self.target_transform)
         if self.target_transform is not None:
             target = self.target_transform(target, width, height)
-            print("target",target)
 
         if self.transform is not None:
             target = np.array(target)
