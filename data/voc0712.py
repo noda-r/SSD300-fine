fe9b2d3685bdd2f4555e6b3489e9a9743398f7c3
noda-r <119392677+noda-r@users.noreply.github.com>
2023-05-31 02:51:50 +0900

    Update voc0712.py

--- data/voc0712.py
+++ data/voc0712.py
@@ -90,7 +90,6 @@ class VOCAnnotationTransform(object):
             res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
             # img_id = target.find('filename').text[:-4]
         # 1画像に複数物体あるので、[物体数,[bndbox]]のリストを作成する
-        print(res[:4])
         return res  # [[xmin, ymin, xmax, ymax, label_ind], ... ]
 
 
